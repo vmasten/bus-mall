6e1345f437f5820d5f1a3d96a7b7faa77bc70ee7
Vince Masten <vmasten@me.com>
2018-07-23 18:04:20 -0700

    logic improvements

--- js/app.js
+++ js/app.js
@@ -4,22 +4,54 @@ var imgs = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg'
 var imgObjs = [];
 
 function ImageTracker(img) {
-  this.name = img.split(/[/*.]/)[1];//took me a while to figure out how to use regex
+  this.name = img.split(/[/*.]/)[1]; //took me a while to figure out how to use regex
   this.path = img;
   this.totalClicks = 0;
+  this.displayed = false;
 }
 
 for(var i = 0; i < imgs.length; i++) {
   imgObjs.push(new ImageTracker(imgs[i]));
 }
 
-var displayImage = document.getElementById('image_picker');
-for (var j = 0; j < 3; i++) {
-  var imgEl = document.createElement('img');
-  var pickerArray = [];
-  pickerArray = imgObjs;
-  var randomImage = Math.floor(Math.random() * (pickerArray.length));
-  imgEl.src = pickerArray[randomImage].path;
-  pickerArray.remove(randomImage);
-  displayImage.appendChild(imgEl);
-}
+var render = function(arr) {
+  console.log(arr);
+  var displayImage = document.getElementById('image_picker');
+  var returnArray = [];
+  for (var i = 0; i < 3; i++) {
+    var imgEl = document.createElement('img');
+    var randomImage = Math.floor(Math.random() * (arr.length));
+    returnArray[i] = arr[randomImage].name;
+    imgEl.src = arr[randomImage].path;
+    displayImage.appendChild(imgEl);
+    arr.splice(randomImage, 1);
+  }
+  return returnArray;
+};
+
+var initDisplay = function() {
+
+  var pickerArray = []; //objects are removed as they're displayed
+  pickerArray = imgObjs.slice();
+  var arr = render(pickerArray);
+  return arr;
+};
+
+var afterDisplay = function(nameArray) {
+  console.log(nameArray);
+  var displayImage = document.getElementById('image_picker');
+  var pickerArray = []; //objects are removed as they're displayed
+  var arr = []; //holds three elements to ensure no repeats
+  pickerArray = imgObjs.slice();
+  for (var i = 0; i < nameArray.length; i++) {
+    for (var j = 0; j < pickerArray.length; j++) {
+      if (nameArray[i] === pickerArray[j].name) {
+        pickerArray.splice(j, 1);
+      }
+    }
+  }
+  arr = render(pickerArray);
+  return arr;
+};
+
+var controlArray = initDisplay();
